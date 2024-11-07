14cc988fae3218c01f44ed5c9d1d365df07c4163
tpktpj <jasmin@tprotocol.io>
2024-11-07 07:03:13 -0700

    Chore: remove tvl on bitcoin chain as lbtc represent the tvl

--- projects/avalon-finance-cedefi/index.js
+++ projects/avalon-finance-cedefi/index.js
@@ -11,14 +11,14 @@ const config = {
 }
 
 // @dev btcOwnersOfCedefi: Bitcoin owners of the Avalon CeDefi pool contract.
-const btcOwnersOfCedefi = [
-'32DgQPVHSV6FSxLnw68nggvchp3ZNKquxA',
-'bc1qr5nz7n8ulcdz0w3s6fska80fawxhvqlh273qypm3rkjequ9wpmhs65ppw7',
-'bc1qhu98nf6ddz6ja73rn72encdr8ezsyhexwpdzap0vcs7lg2wpmrnq5ygfsl',
-'bc1qg6asmzjr7nr5f5upg3xqyrdxl2tq8ef58hha7t0s82mzzx6zjxesyccp4h',
-'bc1qxe3md4lehg8gmrlx3e8xqju5mytt266l4hcy8khl6tm5mahghmeqtxlgqq',
-'bc1qy48h0kuv0r3e330wjfs6r74sk49pkzumnm907t5mhqjxml22r3ss2ucuxc',
-]
+// const btcOwnersOfCedefi = [
+// '32DgQPVHSV6FSxLnw68nggvchp3ZNKquxA',
+// 'bc1qr5nz7n8ulcdz0w3s6fska80fawxhvqlh273qypm3rkjequ9wpmhs65ppw7',
+// 'bc1qhu98nf6ddz6ja73rn72encdr8ezsyhexwpdzap0vcs7lg2wpmrnq5ygfsl',
+// 'bc1qg6asmzjr7nr5f5upg3xqyrdxl2tq8ef58hha7t0s82mzzx6zjxesyccp4h',
+// 'bc1qxe3md4lehg8gmrlx3e8xqju5mytt266l4hcy8khl6tm5mahghmeqtxlgqq',
+// 'bc1qy48h0kuv0r3e330wjfs6r74sk49pkzumnm907t5mhqjxml22r3ss2ucuxc',
+// ]
 
 // @dev getMetrics: call to get the collateral and debt of the Avalon CeDefi pool contract.
 const getMetrics = async (api, borrowed) => {
@@ -45,11 +45,6 @@ module.exports = {
   ethereum: {
     tvl: (api) => getMetrics(api, false),
     borrowed: (api) => getMetrics(api, true),
-  },
-  bitcoin: {
-    tvl: sdk.util.sumChainTvls([
-      sumTokensExport({ owners: btcOwnersOfCedefi }),
-    ]),
   },
 }
 
